cdf7c06f8fbfd80adb43301b9dacb971d79c73a1
Patrik Barkman <patrik.barkman@volumental.com>
2024-12-17 07:31:40 +0100

    Add artist cover to drop down

--- src/lib/components/ArtistsDropDown.svelte
+++ src/lib/components/ArtistsDropDown.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import type { Artist } from '$lib/spotify/api';
+	import { getArtists, type Artist } from '$lib/spotify/api';
+	import { authorizedRequest } from '$lib/spotify/authorization';
+	import { RequestCacher } from '$lib/spotify/cache';
 	import DropDown from './DropDown.svelte';
 
 	interface Props {
@@ -8,10 +10,38 @@
 	}
 
 	let { artists, selected_artists = $bindable() }: Props = $props();
+	let request_cacher = new RequestCacher(authorizedRequest);
+
+	const artists_with_cover = $derived.by(async () => {
+		const artist_ids = artists.map((artist) => artist.id);
+		return await getArtists(artist_ids, request_cacher.makeRequest);
+	});
 </script>
 
-<DropDown placeholder="artists" options={artists} bind:selected={selected_artists}>
-	{#snippet option_snippet(artist: Artist)}
-		{artist.name}
-	{/snippet}
-</DropDown>
+{#await artists_with_cover}
+	<p>loading...</p>
+{:then artists_with_cover}
+	<DropDown placeholder="artists" options={artists_with_cover} bind:selected={selected_artists}>
+		{#snippet option_snippet(artist: Artist)}
+			<artist-option>
+				<img src={artist.cover_url} alt="cover" width="20px" />
+				<div>
+					{artist.name}
+				</div>
+			</artist-option>
+		{/snippet}
+	</DropDown>
+{/await}
+
+<style>
+	artist-option {
+		display: flex;
+		align-items: center;
+		height: 1em;
+		margin: 0.1em;
+	}
+
+	img {
+		margin-right: 0.25em;
+	}
+</style>
